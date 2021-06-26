b3ccc91ec2680defc23a4e6a5984752b086e9b38
Sachintha <sachintha3232@gmail.com>
2021-06-26 19:58:36 -0400

    Initial Commit

---
+++ MoMemory Photo Gallery Cleaner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoMemory Photo Gallery Cleaner
+//
+//  Created by Sachintha Herath on 2021-06-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoMemory Photo Gallery Cleaner/MoMemory_Photo_Gallery_CleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoMemory_Photo_Gallery_CleanerApp.swift
+//  MoMemory Photo Gallery Cleaner
+//
+//  Created by Sachintha Herath on 2021-06-26.
+//
+
+import SwiftUI
+
+@main
+struct MoMemory_Photo_Gallery_CleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
